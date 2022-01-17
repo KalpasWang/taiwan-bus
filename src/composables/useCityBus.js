d9afab4cc8e9074a39308b051914f41e29586f20
KalpasWang <wf6212@gmail.com>
2022-01-17 15:58:22 +0800

    try catch refactoring

--- src/composables/useCityBus.js
+++ src/composables/useCityBus.js
@@ -31,24 +31,17 @@ const state = reactive({
  */
 
 // 取得指定[縣市與路線名稱]的公車資料
-const fetchRoutesByCityAndRouteName = async (city, routeName) => {
-  try {
-    state.error = null
-    state.pending = true
-    let url
-    if (city && !routeName) {
-      url = `Route/City/${city}`
-    } else {
-      url = `Route/City/${city}/${routeName}`
-    }
-    const res = await apiTop30.get(url)
-    // console.log(res.data)
-    state.routesList = res.data
-    state.pending = false
-  } catch (error) {
-    state.error = error.message
-    state.pending = false
+const handleRoutesByCityAndRouteName = async (city, routeName) => {
+  let url
+  if (city && !routeName) {
+    url = `Route/City/${city}`
+  } else {
+    url = `Route/City/${city}/${routeName}`
   }
+  const res = await apiTop30.get(url)
+  // console.log(res.data)
+  state.routesList = res.data
+  state.pending = false
 }
 
 // 取得指定[縣市],[路線名稱]的公車動態定點資料
@@ -135,159 +128,133 @@ const fetchBusNearStop = async (city, routeName) => {
 }
 
 // 取得指定[縣市],[路線名稱]的市區公車路線站序資料與公車預估到站資料
-const fetchStopsAndBusArrivalTime = async (city, routeName) => {
-  try {
-    state.error = null
-    state.pending = true
-    const url = `StopOfRoute/City/${city}/${routeName}`
-    const url2 = `EstimatedTimeOfArrival/City/${city}/${routeName}`
-    // 取得站序資料
-    const res = await api.get(url)
-    // 取得預估時間資料
-    const res2 = await api.get(url2)
+const handleStopsAndBusArrivalTime = async (city, routeName) => {
+  const url = `StopOfRoute/City/${city}/${routeName}`
+  const url2 = `EstimatedTimeOfArrival/City/${city}/${routeName}`
+  // 取得站序資料
+  const res = await api.get(url)
+  // 取得預估時間資料
+  const res2 = await api.get(url2)
 
-    const realData = filterRouteName(routeName, res.data)
-    if (!realData && !realData[0]) {
-      return
-    }
-    const realData2 = filterRouteName(routeName, res2.data)
-    if (!realData2 && !realData2[0]) {
-      return
-    }
-    // 清理 state
-    state.city = city
-    state.routeName = realData[0].RouteName.Zh_tw
-    state.routeUID = realData[0].RouteUID
-    state.forwardStopsList.length = 0
-    state.backwardStopsList.length = 0
-
-    // cache 所需的資料
-    const stopsForward = realData.find((item) => item.Direction === 0).Stops
-    const stopsBackward = realData.find((item) => item.Direction === 1).Stops
-    const timeForward = realData2.filter((item) => item.Direction === 0)
-    const timeBackward = realData2.filter((item) => item.Direction === 1)
-
-    const processStop = (timeArr, stop) => {
-      const time = timeArr.find((item) => item.StopUID === stop.StopUID)
-      let newStop
-      if (time) {
-        const badge = getTimeBadgeAndColor(time)
-        newStop = Object.assign(stop, {
-          EstimateTime: time.EstimateTime,
-          TimeLabel: badge.text,
-          Color: badge.color,
-          BgColor: badge.bgColor,
-          StopStatus: time.StopStatus,
-          Border: badge.border,
-          LinkColor: badge.linkColor
-        })
-      } else {
-        newStop = Object.assign(stop, {
-          TimeLabel: '--',
-          Color: 'text-warning',
-          BgColor: 'bg-dark',
-          StopStatus: 1,
-          Border: false,
-          LinkColor: 'link-light'
-        })
-      }
-      return newStop
-    }
-
-    // 去程
-    stopsForward.forEach((stop) => {
-      const newStop = processStop(timeForward, stop)
-      state.forwardStopsList.push(newStop)
-    })
-
-    // 返程
-    stopsBackward.forEach((stop) => {
-      const newStop = processStop(timeBackward, stop)
-      state.backwardStopsList.push(newStop)
-    })
-
-    await fetchBusNearStop(city, routeName)
-    await fetchBusPosition(city, routeName)
-    await fetchRouteShape(city, routeName)
-
-    state.pending = false
-  } catch (error) {
-    state.error = error.message
-    state.pending = false
+  const realData = filterRouteName(routeName, res.data)
+  if (!realData && !realData[0]) {
+    return
   }
+  const realData2 = filterRouteName(routeName, res2.data)
+  if (!realData2 && !realData2[0]) {
+    return
+  }
+  // 清理 state
+  state.city = city
+  state.routeName = realData[0].RouteName.Zh_tw
+  state.routeUID = realData[0].RouteUID
+  state.forwardStopsList.length = 0
+  state.backwardStopsList.length = 0
+
+  // cache 所需的資料
+  const stopsForward = realData.find((item) => item.Direction === 0).Stops
+  const stopsBackward = realData.find((item) => item.Direction === 1).Stops
+  const timeForward = realData2.filter((item) => item.Direction === 0)
+  const timeBackward = realData2.filter((item) => item.Direction === 1)
+
+  const processStop = (timeArr, stop) => {
+    const time = timeArr.find((item) => item.StopUID === stop.StopUID)
+    let newStop
+    if (time) {
+      const badge = getTimeBadgeAndColor(time)
+      newStop = Object.assign(stop, {
+        EstimateTime: time.EstimateTime,
+        TimeLabel: badge.text,
+        Color: badge.color,
+        BgColor: badge.bgColor,
+        StopStatus: time.StopStatus,
+        Border: badge.border,
+        LinkColor: badge.linkColor
+      })
+    } else {
+      newStop = Object.assign(stop, {
+        TimeLabel: '--',
+        Color: 'text-warning',
+        BgColor: 'bg-dark',
+        StopStatus: 1,
+        Border: false,
+        LinkColor: 'link-light'
+      })
+    }
+    return newStop
+  }
+
+  // 去程
+  stopsForward.forEach((stop) => {
+    const newStop = processStop(timeForward, stop)
+    state.forwardStopsList.push(newStop)
+  })
+
+  // 返程
+  stopsBackward.forEach((stop) => {
+    const newStop = processStop(timeBackward, stop)
+    state.backwardStopsList.push(newStop)
+  })
+
+  await fetchBusNearStop(city, routeName)
+  await fetchBusPosition(city, routeName)
+  await fetchRouteShape(city, routeName)
 }
 
 // 取得指定[位置,範圍]的全臺公車站位資料
-const fetchNearByStations = (radius) => {
+const handleNearByStations = (radius) => {
   if (!navigator.geolocation) {
     state.error = 'Geolocation is not supported by your browser'
     return
   }
   navigator.geolocation.watchPosition(async (position) => {
-    try {
-      state.error = null
-      state.pending = true
-      const lat = position.coords.latitude
-      const lng = position.coords.longitude
-      const url = 'Station/NearBy'
-      const res = await apiTop30.get(url, {
-        params: {
-          $spatialFilter: `nearby(${lat}, ${lng}, ${radius})`
+    const lat = position.coords.latitude
+    const lng = position.coords.longitude
+    const url = 'Station/NearBy'
+    const res = await apiTop30.get(url, {
+      params: {
+        $spatialFilter: `nearby(${lat}, ${lng}, ${radius})`
+      }
+    })
+    state.stationsList = []
+    res.data.forEach((item) => {
+      const d = haversine(
+        { lat, lng },
+        {
+          lat: item.StationPosition.PositionLat,
+          lng: item.StationPosition.PositionLon
         }
+      )
+      state.stationsList.push({
+        Distance: Math.round(d),
+        ...item
       })
-      state.stationsList = []
-      res.data.forEach((item) => {
-        const d = haversine(
-          { lat, lng },
-          {
-            lat: item.StationPosition.PositionLat,
-            lng: item.StationPosition.PositionLon
-          }
-        )
-        state.stationsList.push({
-          Distance: Math.round(d),
-          ...item
-        })
-      })
-      state.stationsList.sort((a, b) => a.Distance - b.Distance)
-      // console.log(res.data)
-      state.pending = false
-    } catch (error) {
-      state.error = error.message
-      // console.log(state.error)
-      state.pending = false
-    }
+    })
+    state.stationsList.sort((a, b) => a.Distance - b.Distance)
+    // console.log(res.data)
   })
 }
 
 // 取得指定[縣市]與站位ID的市區公車站位資料
-const fetchStation = async (citys, stationId) => {
-  try {
-    state.error = null
-    state.pending = true
-    const url = `Station/City/${citys[0]}`
-    let res = await api.get(url, {
-      params: {
-        $filter: `StationID eq '${stationId}'`
-      }
+const handleStation = async (citys, stationId) => {
+  const url = `Station/City/${citys[0]}`
+  let res = await api.get(url, {
+    params: {
+      $filter: `StationID eq '${stationId}'`
+    }
+  })
+  state.station = res.data[0]
+  state.station.destination = []
+  citys.forEach(async (city) => {
+    const url = `StopOfRoute/City/${city}/PassThrough/Station/${stationId}`
+    res = await api.get(url)
+    res.data.forEach((route) => {
+      const stops = route.Stops
+      const finalStop = stops[stops.length - 1]
+      finalStop.RouteName = route.RouteName
+      state.station.destination.push(finalStop)
     })
-    state.station = res.data[0]
-    state.station.destination = []
-    citys.forEach(async (city) => {
-      const url = `StopOfRoute/City/${city}/PassThrough/Station/${stationId}`
-      res = await api.get(url)
-      res.data.forEach((route) => {
-        const stops = route.Stops
-        const finalStop = stops[stops.length - 1]
-        finalStop.RouteName = route.RouteName
-        state.station.destination.push(finalStop)
-      })
-    })
-    state.pending = false
-  } catch (error) {
-    state.error = error.message
-    state.pending = false
-  }
+  })
 }
 
 // 取得指定[縣市],[站位]的市區公車預估到站資料
@@ -296,6 +263,30 @@ const fetchEstimatedTimeOfArrivalByStaionId = async (city, stationId) => {
   const res = await api.get(url)
   return res
 }
+
+const tryCatchFactory = (handler) => {
+  return async (a, b) => {
+    try {
+      state.error = null
+      state.pending = true
+      await handler(a, b)
+      state.pending = false
+    } catch (error) {
+      state.error = error.message
+      state.pending = false
+    }
+  }
+}
+
+const fetchRoutesByCityAndRouteName = tryCatchFactory(
+  handleRoutesByCityAndRouteName
+)
+const fetchStopsAndBusArrivalTime = tryCatchFactory(
+  handleStopsAndBusArrivalTime
+)
+const fetchNearByStations = tryCatchFactory(handleNearByStations)
+
+const fetchStation = tryCatchFactory(handleStation)
 
 export default {
   state: readonly(state),
