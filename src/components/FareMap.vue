6028dd398c62c49e5c926518231e03d6ab0f5fe9
KalpasWang <wf6212@gmail.com>
2022-08-12 23:43:25 +0800

    add FareMap fetch data test

--- src/components/FareMap.vue
+++ src/components/FareMap.vue
@@ -3,7 +3,10 @@
 </template>
 
 <script setup>
-defineProps({
+import { ref } from 'vue'
+import { api } from '../composables/api'
+import { filterRouteName, filterDirection } from '../composables/useUtilities'
+const props = defineProps({
   routeName: {
     type: String,
     required: true,
@@ -15,6 +18,24 @@ defineProps({
     default: 'forward'
   }
 })
+
+const result = ref(null)
+const city = null
+const routeName = props.routeName
+
+// 取得指定[路線名稱]的公車/客運路線票價資料
+const fetchRouteFare = async () => {
+  // 設定要 fetch 的網址
+  let url = `RouteFare/City/${city}`
+  if (!city) {
+    url = 'RouteFare/InterCity'
+  }
+  const res = await api.get(`${url}/${routeName}`)
+  console.log(res.data)
+  result.value = filterRouteName(routeName, res.data)
+  console.log(result.value)
+}
+fetchRouteFare()
 </script>
 
 <style lang="scss" scoped></style>
